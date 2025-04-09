ad59a8bfc3a8c6fbed46b48f8c7f3855dab036e7
JaeHwan <wpghks56@naver.com>
2025-04-09 15:49:41 +0900

    [Feature] medium button test code add

--- test/core/presentation/components/medium_button_test.dart
+++ test/core/presentation/components/medium_button_test.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:recipe_app/core/presentation/components/big_button.dart';
+import 'package:recipe_app/core/presentation/components/medium_button.dart';
 
 void main() {
-  group('큰 버튼 테스트', () {
-    testWidgets('글자 테스트', (tester) async {
-      const String testText = 'Big Button';
+  group('보통 버튼 테스트', () {
+    const String testText = 'Medium Button';
+    int testValue = 0;
 
+    testWidgets('글자 테스트', (tester) async {
       await tester.pumpWidget(
-        MaterialApp(home: BigButton(text: testText, onClick: () {})),
+        MaterialApp(home: MediumButton(text: testText, onClick: () {})),
       );
 
       final Finder finder = find.text(testText);
@@ -16,12 +17,9 @@ void main() {
       expect(finder, findsNWidgets(1));
     });
     testWidgets('콜백 테스트', (tester) async {
-      const String testText = 'Big Button';
-      int testValue = 0;
-
       await tester.pumpWidget(
         MaterialApp(
-          home: BigButton(
+          home: MediumButton(
             text: testText,
             onClick: () {
               testValue++;
@@ -30,7 +28,7 @@ void main() {
         ),
       );
 
-      final Finder finder = find.byType(BigButton);
+      final Finder finder = find.byType(MediumButton);
 
       await tester.tap(finder);
       await tester.pump();
