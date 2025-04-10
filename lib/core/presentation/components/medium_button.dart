7fb6848faeccaa5b54aae22fe0be47322ec445e1
JaeHwan <wpghks56@naver.com>
2025-04-10 14:57:44 +0900

    [Fix] medium button component tap action add

--- lib/core/presentation/components/medium_button.dart
+++ lib/core/presentation/components/medium_button.dart
@@ -2,20 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/ui/color_styles.dart';
 import 'package:recipe_app/ui/text_styles.dart';
 
-class MediumButton extends StatelessWidget {
+class MediumButton extends StatefulWidget {
   final String text;
   final VoidCallback onClick;
 
   const MediumButton({super.key, required this.text, required this.onClick});
 
   @override
+  State<MediumButton> createState() => _MediumButtonState();
+}
+
+class _MediumButtonState extends State<MediumButton> {
+  bool isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onClick,
+      onTapDown: (down) {
+        setState(() {
+          isPressed = true;
+        });
+      },
+      onTapUp: (up) {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      onTap: widget.onClick,
       child: Container(
         height: 54,
         decoration: BoxDecoration(
-          color: ColorStyle.primary100,
+          color: isPressed ? ColorStyle.gray4 : ColorStyle.primary100,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Row(
@@ -24,7 +46,7 @@ class MediumButton extends StatelessWidget {
             SizedBox(
               width: 114,
               child: Text(
-                text,
+                widget.text,
                 style: AppTextStyles.normalBold.copyWith(
                   color: ColorStyle.white,
                 ),
