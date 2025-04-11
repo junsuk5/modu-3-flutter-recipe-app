b9b5650d32911f49a40b67218d18c78eb2267d0d
JaeHwan <wpghks56@naver.com>
2025-04-11 16:12:29 +0900

    [Feature] splash screen and scaffold to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:recipe_app/data/data_source/recipe_data_source_impl.dart';
 import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
 import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
 import 'package:recipe_app/presentation/saved_recipes/screen/saved_recipes_root.dart';
+import 'package:recipe_app/presentation/splash/splash_screen.dart';
 import 'package:recipe_app/ui/color_styles.dart';
 
 void main() {
@@ -20,7 +21,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: ColorStyle.primary60),
       ),
-      home: const Scaffold(body: NewWidget()),
+      home: const Scaffold(body: SplashWidget()),
+    );
+  }
+}
+
+class SplashWidget extends StatelessWidget {
+  const SplashWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      onStartCooking: () {
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => const NewWidget()));
+      },
     );
   }
 }
@@ -30,36 +46,38 @@ class NewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const ComponentTestScreen(),
-                ),
-              );
-            },
-            child: const Text('Components'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder:
-                      (context) => SavedRecipeRoot(
-                        viewModel: SavedRecipesViewModel(
-                          RecipeRepositoryImpl(RecipeDataSourceImpl()),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ComponentTestScreen(),
+                  ),
+                );
+              },
+              child: const Text('Components'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder:
+                        (context) => SavedRecipeRoot(
+                          viewModel: SavedRecipesViewModel(
+                            RecipeRepositoryImpl(RecipeDataSourceImpl()),
+                          ),
                         ),
-                      ),
-                ),
-              );
-            },
-            child: const Text('Saved Recipes'),
-          ),
-        ],
+                  ),
+                );
+              },
+              child: const Text('Saved Recipes'),
+            ),
+          ],
+        ),
       ),
     );
   }
