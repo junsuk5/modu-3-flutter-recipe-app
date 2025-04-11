73cb17e0f3403bba15809389b7265c91bbe6c647
JaeHwan <wpghks56@naver.com>
2025-04-11 15:45:08 +0900

    [Fix] 레시피 화면에 탐색 기능을 추가하고 테마 색상을 업데이트합니다

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/core/presentation/components/component_test_screen.dart';
+import 'package:recipe_app/data/data_source/recipe_data_source_impl.dart';
+import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
+import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
+import 'package:recipe_app/presentation/saved_recipes/screen/saved_recipes_root.dart';
+import 'package:recipe_app/ui/color_styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +18,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: ColorStyle.primary60),
       ),
-      home: ComponentTestScreen(),
+      home: const Scaffold(body: NewWidget()),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const ComponentTestScreen(),
+                ),
+              );
+            },
+            child: const Text('Components'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder:
+                      (context) => SavedRecipeRoot(
+                        viewModel: SavedRecipesViewModel(
+                          RecipeRepositoryImpl(RecipeDataSourceImpl()),
+                        ),
+                      ),
+                ),
+              );
+            },
+            child: const Text('Saved Recipes'),
+          ),
+        ],
+      ),
     );
   }
 }
