fdb5798306fc702c325afaef72975d9a516f7f71
JaeHwan <wpghks56@naver.com>
2025-04-11 21:06:58 +0900

    Refactor navigation to use named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/core/presentation/components/component_test_screen.dart';
-import 'package:recipe_app/data/data_source/recipe_data_source_impl.dart';
-import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
-import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
-import 'package:recipe_app/presentation/saved_recipes/screen/saved_recipes_root.dart';
-import 'package:recipe_app/presentation/splash/splash_screen.dart';
 import 'package:recipe_app/ui/color_styles.dart';
+
+import 'utils/constant/default_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,66 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      routes: DefaultRoute.routes,
+      initialRoute: '/splash',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: ColorStyle.primary60),
-      ),
-      home: const Scaffold(body: SplashWidget()),
-    );
-  }
-}
-
-class SplashWidget extends StatelessWidget {
-  const SplashWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      onStartCooking: () {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const NewWidget()),
-        );
-      },
-    );
-  }
-}
-
-class NewWidget extends StatelessWidget {
-  const NewWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ComponentTestScreen(),
-                  ),
-                );
-              },
-              child: const Text('Components'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder:
-                        (context) => SavedRecipeRoot(
-                          viewModel: SavedRecipesViewModel(
-                            RecipeRepositoryImpl(RecipeDataSourceImpl()),
-                          ),
-                        ),
-                  ),
-                );
-              },
-              child: const Text('Saved Recipes'),
-            ),
-          ],
-        ),
       ),
     );
   }
---
+++ lib/utils/constant/default_route.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+import 'package:recipe_app/presentation/test/component_test_screen.dart';
+import 'package:recipe_app/data/data_source/recipe_data_source_impl.dart';
+import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
+import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
+import 'package:recipe_app/presentation/saved_recipes/screen/saved_recipes_root.dart';
+import 'package:recipe_app/presentation/splash/splash_screen.dart';
+import 'package:recipe_app/presentation/test/test_screen.dart';
+
+abstract class DefaultRoute {
+  static Map<String, WidgetBuilder> routes = {
+    '/splash':
+        (context) => SplashScreen(
+          onStartCooking: () {
+            Navigator.of(context).pushReplacementNamed('/test');
+          },
+        ),
+    '/test':
+        (context) => TestScreen(
+          onComponentButton: () {
+            Navigator.of(context).pushNamed('/components');
+          },
+          onSavedRecipeButton: () {
+            Navigator.of(context).pushNamed('/savedRecipes');
+          },
+        ),
+    '/components': (context) => const ComponentTestScreen(),
+    '/savedRecipes':
+        (context) => SavedRecipeRoot(
+          viewModel: SavedRecipesViewModel(
+            RecipeRepositoryImpl(RecipeDataSourceImpl()),
+          ),
+        ),
+  };
+}
