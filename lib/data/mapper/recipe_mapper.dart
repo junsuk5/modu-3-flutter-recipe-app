3ef44f478a70573647d2ff5bb441214fcc33bca2
JaeHwan <wpghks56@naver.com>
2025-04-11 15:45:31 +0900

    [Feature] Add Recipe mapper to convert RecipeDto to Recipe model

---
+++ lib/data/mapper/recipe_mapper.dart
@@ -0,0 +1,18 @@
+import 'package:recipe_app/data/dto/recipe_dto.dart';
+import 'package:recipe_app/data/enums/category.dart';
+import 'package:recipe_app/data/mapper/ingredients_mapper.dart';
+import 'package:recipe_app/domain/model/recipe.dart';
+
+extension RecipeMapper on RecipeDto {
+  Recipe toRecipe() {
+    return Recipe(
+      category: category ?? Category.unknown,
+      chef: chef ?? '정보 없음',
+      foodName: name ?? '정보 없음',
+      image: image ?? '',
+      ingredients: ingredients?.map((e) => e.toIngredients()).toList() ?? [],
+      rate: rating ?? -1.1,
+      time: time ?? '정보 없음',
+    );
+  }
+}
