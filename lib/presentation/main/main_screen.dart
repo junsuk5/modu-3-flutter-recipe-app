1e0bc9e1112b4270fee37c08b46f7c1fe371b8c3
JaeHwan <wpghks56@naver.com>
2025-04-15 15:54:23 +0900

    [Feature] main screen create

---
+++ lib/presentation/main/main_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:recipe_app/ui/color_styles.dart';
+
+class MainScreen extends StatelessWidget {
+  final StatefulNavigationShell body;
+  final int currentPageIndex;
+  final void Function(int value) onChangeIndex;
+  const MainScreen({
+    super.key,
+    required this.body,
+    required this.currentPageIndex,
+    required this.onChangeIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: body,
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: ColorStyle.white,
+        onDestinationSelected: onChangeIndex,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        selectedIndex: currentPageIndex,
+        indicatorColor: Colors.transparent,
+        destinations: [
+          NavigationDestination(
+            icon: Image.asset('assets/images/home-2.png'),
+            label: 'home',
+            selectedIcon: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/home-2-1.png',
+                  color: ColorStyle.primary100.withValues(alpha: 0.7),
+                ),
+              ],
+            ),
+          ),
+          NavigationDestination(
+            icon: Image.asset('assets/images/Inactive.png'),
+            label: 'home',
+            selectedIcon: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/Active.png',
+                  color: ColorStyle.primary100.withValues(alpha: 0.7),
+                ),
+              ],
+            ),
+          ),
+          NavigationDestination(
+            icon: Image.asset('assets/images/notification-bing.png'),
+            label: 'home',
+            selectedIcon: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/notification-bing.png',
+                  color: ColorStyle.primary100.withValues(alpha: 0.7),
+                ),
+              ],
+            ),
+          ),
+          NavigationDestination(
+            icon: Image.asset('assets/images/profile.png'),
+            label: 'home',
+            selectedIcon: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/frame.png',
+                  color: ColorStyle.primary100.withValues(alpha: 0.7),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
