11862e81377e98fe11ef35e6433309c97532ec80
JaeHwan <wpghks56@naver.com>
2025-04-15 13:55:15 +0900

    [Feature] routes.dart create ;

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,9 @@
+abstract class Routes {
+  static const String splash = '/splsh';
+  static const String signIn = '/sign-in';
+  static const String signUp = '/sign-up';
+
+  static const String home = '/home';
+  static const String savedRecipes = '/saved-recipes';
+  static const String searchRecipes = '/search-recipes';
+}
