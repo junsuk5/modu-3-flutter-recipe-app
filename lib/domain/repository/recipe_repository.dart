4fca9c51ec80e7b990f3342bc462ef175b447d74
JaeHwan <wpghks56@naver.com>
2025-04-16 17:01:40 +0900

    Add bookmark management and update recipe repository
    The body adds additional context about implementation details:
    
    Adds findRecipeById method to repository interface
    Replaces direct repository usage with use case pattern
    Implements bookmark add/remove functionality

--- lib/domain/repository/recipe_repository.dart
+++ lib/domain/repository/recipe_repository.dart
@@ -1,6 +1,9 @@
 import 'package:recipe_app/domain/model/recipe.dart';
 import 'package:recipe_app/domain/repository/base_repository.dart';
 import 'package:recipe_app/utils/errors/recipe_error_enum.dart';
+import 'package:recipe_app/utils/result/result.dart';
 
 abstract interface class RecipeRepository
-    implements BaseRepository<Recipe, RecipeErrorEnum> {}
+    implements BaseRepository<Recipe, RecipeErrorEnum> {
+  Future<Result<Recipe, RecipeErrorEnum>> findRcipeById(int id);
+}
--- lib/presentation/saved_recipes/saved_recipes_view_model.dart
+++ lib/presentation/saved_recipes/saved_recipes_view_model.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/domain/model/recipe.dart';
-import 'package:recipe_app/domain/repository/recipe_repository.dart';
+import 'package:recipe_app/domain/use_case/get_saved_recipes_use_case.dart';
 import 'package:recipe_app/presentation/saved_recipes/saved_recipes_state.dart';
 import 'package:recipe_app/utils/errors/recipe_error_enum.dart';
 import 'package:recipe_app/utils/result/result.dart';
 
 class SavedRecipesViewModel with ChangeNotifier {
-  final RecipeRepository _repository;
-  SavedRecipesState _state = SavedRecipesState();
+  final GetSavedRecipesUseCase _savedRecipesUseCase;
+  SavedRecipesState _state = const SavedRecipesState();
 
   SavedRecipesState get state => _state;
 
-  SavedRecipesViewModel(RecipeRepository repository) : _repository = repository;
+  SavedRecipesViewModel(this._savedRecipesUseCase);
 
   Future<void> findRecipes() async {
     _state = state.copyWith(isLoading: true);
     notifyListeners();
 
-    final result = await _repository.findDatas();
+    final result = await _savedRecipesUseCase.execute();
 
     switch (result) {
       case Success<List<Recipe>, RecipeErrorEnum>():
@@ -27,5 +27,16 @@ class SavedRecipesViewModel with ChangeNotifier {
         _state = state.copyWith(error: result.error, isLoading: false);
         notifyListeners();
     }
+  }
+
+  Future<void> addBookmark(int id) async {
+    await _savedRecipesUseCase.bookmarkRepository.save(id);
+    notifyListeners();
+  }
+
+  Future<void> removeBookmark(int id) async {
+    await _savedRecipesUseCase.bookmarkRepository.unSave(id);
+    await findRecipes();
+    notifyListeners();
   }
 }
